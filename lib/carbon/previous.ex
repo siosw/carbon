f8613f08287ab956586abef2b2a6b6d1cae1e795
Simon Oswald <simon.oswald@adjust.com>
2021-05-21 12:29:57 +0200

    previous now starts downloading from last known date

--- lib/carbon/previous.ex
+++ lib/carbon/previous.ex
@@ -1,10 +1,12 @@
 defmodule Carbon.Previous do
+  import Ecto.Query, only: [from: 2]
+
   alias Carbon.Storage
 
   # bench: 27.441625s for 100 days
   def get_sync(days) do
     0..-days
-    |> Enum.map(&Date.add(Date.utc_today(), &1))
+    |> Enum.map(&Date.add(last_known_date(), &1))
     |> Enum.map(&Date.to_string/1)
     |> Enum.map(&Storage.store_date/1)
     |> Enum.sum()
@@ -13,11 +15,21 @@ defmodule Carbon.Previous do
   # bench: 2.894843s for 100 days
   def get_async(days) do
     0..-days
-    |> Enum.map(&Date.add(Date.utc_today(), &1))
+    |> Enum.map(&Date.add(last_known_date(), &1))
     |> Enum.map(&Date.to_string/1)
     |> Enum.map(&spawn_store_date/1)
     |> Enum.map(&receive_result/1)
     |> Enum.sum()
+  end
+
+  def last_known_date() do
+    sq = from s_d in Carbon.Intensity, select: min(s_d.to)
+    from(t in Carbon.Intensity, where: t.to in subquery(sq), select: t.to)
+    |> Carbon.Repo.one!()
+    |> case do
+      nil -> Date.utc_today()
+      timestamp -> DateTime.to_date(timestamp)
+    end
   end
 
   defp receive_result(_pid) do
